xiaomi-sm8450-mondrian/android_packages_apps_Settings

[RisingUI] Revamp personalization dashboard
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -405,6 +405,11 @@ public class SettingsGateway {
             LockScreen.class.getName(),
             Themes.class.getName(),
             Toolbox.class.getName(),
+            MonetSettings.class.getName(),
+            com.rising.settings.fragments.ui.fonts.FontPickerPreview.class.getName(),
+            Notifications.class.getName(),
+            Sound.class.getName(),
+            com.rising.settings.fragments.ui.IconShapes.class.getName(),
             SmartPixels.class.getName(),
             RunningServices.class.getName()
     };
@@ -457,6 +462,11 @@ public class SettingsGateway {
             Settings.PersonalizationsLockscreenActivity.class.getName(),
             Settings.PersonalizationsToolboxActivity.class.getName(),
             Settings.PersonalizationsThemesActivity.class.getName(),
+            Settings.PersonalizationsColorsActivity.class.getName(),
+            Settings.PersonalizationsFontsActivity.class.getName(),
+            Settings.PersonalizationsSoundsActivity.class.getName(),
+            Settings.PersonalizationsIconsActivity.class.getName(),
+            Settings.PersonalizationsNotificationsActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName()
     };

Arman-experiments/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -438,7 +438,12 @@ public class SettingsGateway {
             PersonalizationsFragment.class.getName(),
             LockScreen.class.getName(),
             Themes.class.getName(),
-            Toolbox.class.getName()
+            Toolbox.class.getName(),
+            MonetSettings.class.getName(),
+            com.rising.settings.fragments.ui.fonts.FontPickerPreview.class.getName(),
+            Notifications.class.getName(),
+            Sound.class.getName(),
+            com.rising.settings.fragments.ui.IconShapes.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -489,6 +494,11 @@ public class SettingsGateway {
             Settings.PersonalizationsActivity.class.getName(),
             Settings.PersonalizationsLockscreenActivity.class.getName(),
             Settings.PersonalizationsToolboxActivity.class.getName(),
-            Settings.PersonalizationsThemesActivity.class.getName()
+            Settings.PersonalizationsThemesActivity.class.getName(),
+            Settings.PersonalizationsColorsActivity.class.getName(),
+            Settings.PersonalizationsFontsActivity.class.getName(),
+            Settings.PersonalizationsSoundsActivity.class.getName(),
+            Settings.PersonalizationsIconsActivity.class.getName(),
+            Settings.PersonalizationsNotificationsActivity.class.getName()
     };
 }